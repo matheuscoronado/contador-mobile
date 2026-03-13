504c82b65af15e33b4fd3e94daeb24396d648d2a
Matheus Coronado <matheuscoronado48@gmail.com>
2026-03-13 19:54:54 -0300

    Criado botões para Incrementar e Decrementar os contadores, com regras de mudanças de cores ao atingir números previamente determinados

--- app/src/main/java/com/coronado/contador_mobile/MainActivity.java
+++ app/src/main/java/com/coronado/contador_mobile/MainActivity.java
@@ -1,24 +1,68 @@
 package com.coronado.contador_mobile;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+
+    // Define as variáveis globais (objetos de tela)
+    private int contador = 0;
+    private TextView tvContador;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        // Vincula os componentes XML com Java
+        tvContador = findViewById(R.id.tvContador);
+        Button btnInc = findViewById(R.id.btnIncrementar);
+        Button btnDec = findViewById(R.id.btnDecrementar);
+
+        // Eventos de click para incrementar
+        btnInc.setOnClickListener(v -> {
+            contador++;
+            atualizarInterface();
+
+
+
+
+            //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            //Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            //v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            //return insets;
         });
+
+        // Evento de click para decrementar
+
+        btnDec.setOnClickListener(v -> {
+            if(contador > 0) {
+                contador--;
+                atualizarInterface();
+            }
+        });
+    }
+
+    // Metodo para centralizar e e atualizar a UI
+    private void atualizarInterface(){
+        tvContador.setText(String.valueOf(contador));
+
+        //Regra de mudança de cor ao atingir limite de 10
+        if(contador <= 9){
+            tvContador.setTextColor(Color.BLACK);
+        } else if (contador >= 10 && contador < 20) {
+            tvContador.setTextColor(Color.BLUE);
+        } else{
+            tvContador.setTextColor(Color.GREEN);
+        }
+
     }
 }
